400f4f2aa11d0b3b09bbe3719e17ce2f774aa47f
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-08 20:56:35 +0330

    create blog app and urls

---
+++ homepage/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from homepage.views import *
+
+urlpatterns = [
+    path('', index_view ,name='index'),
+    path('about', about_view,name='about' ),
+    path('contact', contact_view ,name='contact'),
+
+]
