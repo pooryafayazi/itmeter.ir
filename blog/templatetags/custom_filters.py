9e65ce6f7b478f6bf27544d782c75aa95816beb6
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-17 20:40:46 +0330

    popular categories

--- blog/templatetags/custom_filters.py
+++ blog/templatetags/custom_filters.py
@@ -8,3 +8,37 @@ from django.utils import timezone
 def latest_posts(arg=3):
     posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-counted_views')[:arg]
     return {'posts' : posts}
+
+'''
+@register.inclusion_tag('categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now())
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' : cat_dict}
+'''
+
+'''
+@register.inclusion_tag('home/categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now())
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' : cat_dict}'''
+
+
+@register.inclusion_tag('home/categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now())
+    categories = Category.objects.all()
+    
+    cat_list = [] 
+    for category in categories:
+        count = posts.filter(category=category).count() 
+        cat_list.append({'name': category.name, 'count': count}) 
+    cat_list.sort(key=lambda x: x['count'], reverse=True)
+    return {'categories': cat_list} 
--- homepage/templatetags/template_tag.py
+++ homepage/templatetags/template_tag.py
@@ -12,6 +12,7 @@ def truncate_w(text, word_count):
     return text
 
 
+
 @register.simple_tag
 def get_latest_news(count=6):
     return News.objects.order_by('-published_date')[:count].filter(status=1, published_date__lte=timezone.now())
@@ -26,11 +27,39 @@ def latest_news(arg=3):
 def function(news_id):
     return newsComment.objects.filter(News=news_id, approved=True).count()
 
-@register.inclusion_tag('blog/blog-News-category.html')
-def postcategories():
+'''
+@register.simple_tag
+def newscategories():
     news = News.objects.filter(status=1, published_date__lte=timezone.now())
     categories = Category.objects.all()
     cat_dict = {}
     for name in categories:
         cat_dict[name] = news.filter(category=name).count()
     return {'categories' : cat_dict}
+'''
+
+
+'''
+@register.inclusion_tag('home/categories.html')
+def newscategories():
+    posts = News.objects.filter(status=1, published_date__lte=timezone.now())
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' : cat_dict}'''
+
+
+@register.inclusion_tag('home/categories.html')
+def newscategories():
+    news = News.objects.filter(status=1, published_date__lte=timezone.now())
+    categories = Category.objects.all()
+    
+    cat_list = [] 
+    for category in categories:
+        count = news.filter(category=category).count() 
+        cat_list.append({'name': category.name, 'count': count}) 
+    cat_list.sort(key=lambda x: x['count'], reverse=True)
+    return {'categories': cat_list}
+
+
