01f6e956775b10508e74ff738e0d1d73afc2bc67
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-18 19:21:36 +0330

    09.05

--- itmeter/urls.py
+++ itmeter/urls.py
@@ -39,7 +39,8 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('blog/', include('blog.urls')),
     
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",)
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",),
+    path('robots.txt', include('robots.urls')),
 
 ]
 
