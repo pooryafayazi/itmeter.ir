3af314e262cd3dc1370518ae8a581aa61a3e1994
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-20 12:09:26 +0330

    10.03

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
 # Create your views here.
-
+from django.contrib.auth import authenticate, login
 
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+
+
+    if not request.user.is_authenticated:      
+
+        return render(request, 'accounts/login.html')
 
 
 #def logout_view(request):
